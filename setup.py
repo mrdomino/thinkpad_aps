326af98b080d8e46f418d6dceef65a6d817e80e8
Steven Dee <steve@smartercode.net>
2013-06-25 17:13:19 -0400

    Better description

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ module1 = Extension('aps', sources = ['aps.c', 'pyaps.c'])
 
 setup(name = 'thinkpad_aps',
       version = '0.0',
-      description = 'Thinkpad APS accelerometer data',
+      description = 'Small OpenBSD Thinkpad Active Protection System (accelerometer) library',
       ext_modules = [module1])
